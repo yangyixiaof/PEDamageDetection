2cd5390a1e17b7b22a97cbb37cf93098e2ab6340
yyx <yangyixiaofirst@163.com>
2016-08-03 15:36:13 +0800

    small change.

--- UVCStreamHandler/src/UVCUI/UVCJFrame.java
+++ UVCStreamHandler/src/UVCUI/UVCJFrame.java
@@ -16,10 +16,10 @@ public class UVCJFrame extends JFrame {
 
 	private static final long serialVersionUID = -6026250728018827459L;
 	
-	private JButton control = new JButton("Start");
-	private JPanel canvas = new JPanel();
-	private JPanel bigimageshow = new JPanel();
-	private JPanel thumbnails = new JPanel();
+	private JButton startcontrol = new JButton("Start");
+	private JButton stopcontrol = new JButton("Stop");
+	private JPanel canvasvideo = new JPanel();
+	private JPanel canvasimage = new JPanel();
 	
 	private ArrayList<JLabel> labels = new ArrayList<JLabel>();
 	
@@ -30,59 +30,53 @@ public class UVCJFrame extends JFrame {
 	 */
 	public UVCJFrame() {
 		setLayout(null);
-		setSize(800, 450);
+		setSize(1000, 450);
 		setLocationRelativeTo(null);
 		setResizable(false);
 		setVisible(true);
 		setTitle("Camera JFrame");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		canvas.setBounds(0, 0, 480, 400);
-		getContentPane().add(canvas);
+		canvasvideo.setBounds(10, 0, 480, 400);
+		getContentPane().add(canvasvideo);
 		
-		bigimageshow.setBounds(500, 0, 300, 250);
-		getContentPane().add(bigimageshow);
+		canvasimage.setBounds(500, 0, 480, 400);
+		getContentPane().add(canvasimage);
 		
-		thumbnails.setBounds(500, 295, 300, 100);
-		getContentPane().add(thumbnails);
+		startcontrol.setBounds(350, 415, 50, 20);
+		getContentPane().add(startcontrol);
 		
-		control.setBounds(430, 780, 20, 20);
-		getContentPane().add(control);
+		stopcontrol.setBounds(650, 415, 50, 20);
+		getContentPane().add(stopcontrol);
 		
-		for (int i=0;i<5;i++)
-		{
-			JLabel jl = new JLabel();
-			jl.setBounds(i*55, 25, 54, 45);
-			jl.setHorizontalAlignment(0);
-			// jl.setIcon(icon);
-			thumbnails.add(jl);
-			labels.add(jl);
-		}
-		
-		control.addActionListener(new java.awt.event.ActionListener() {
+		startcontrol.addActionListener(new java.awt.event.ActionListener() {
 			@Override
 			public void actionPerformed(java.awt.event.ActionEvent evt) {
 				try {
-					ControlActionPerformed(evt);
+					StartControlActionPerformed(evt);
 				} catch (Exception ex) {
 					ex.printStackTrace();
 				}
 			}
 		});
-		// pack();
+		
+		stopcontrol.addActionListener(new java.awt.event.ActionListener() {
+			@Override
+			public void actionPerformed(java.awt.event.ActionEvent evt) {
+				StopControlActionPerformed(evt);
+			}
+		});
 	}
 	
-	private void ControlActionPerformed(ActionEvent evt)
+	private void StartControlActionPerformed(ActionEvent evt)
 			throws Exception, FrameGrabber.Exception, InterruptedException {
-		if (control.getText().equals("Stop")) {
-			uvcst.Stop();
-			control.setText("Start");
-		} else {
-			control.setText("Stop");
-			uvcst = new UVCStreamTask(canvas, bigimageshow, labels);
-			Thread uvctd = new Thread(uvcst);
-			uvctd.start();
-		}
+		uvcst = new UVCStreamTask(canvasvideo, canvasimage, labels);
+		Thread uvctd = new Thread(uvcst);
+		uvctd.start();
+	}
+	
+	private void StopControlActionPerformed(ActionEvent evt) {
+		uvcst.Stop();
 	}
 	
 	public static void main(String[] args) {
